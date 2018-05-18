dbb7d4bbe7fcb2974f36840ceada21ebe90af993
Michelle Wise <michellet@vippayroll.co.za>
2018-05-18 10:44:20 +0200

    Add project files.

---
+++ RunnersLog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RunnersLog.Startup))]
+namespace RunnersLog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
